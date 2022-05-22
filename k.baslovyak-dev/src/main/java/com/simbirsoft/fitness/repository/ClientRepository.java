93b8f7f43c536a06ffc04ab9078268b6c2c7203d
imaksimkin <int-8@yandex.ru>
2022-05-22 13:12:41 +0300

    init

--- k.baslovyak-dev/src/main/java/com/simbirsoft/fitness/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simbirsoft.fitness.repository;
-
-import com.simbirsoft.fitness.model.user.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client,Long> {
-}
--- k.baslovyak-dev/src/main/java/com/simbirsoft/fitness/repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simbirsoft.fitness.repository;
-
-import com.simbirsoft.fitness.model.user.Coach;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoachRepository extends JpaRepository<Coach, Long> {
-}
